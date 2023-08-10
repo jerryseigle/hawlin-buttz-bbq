7b1b76322febc65bebda080553d54b024df3581b
jerryseigle <mr.jerryseigle@gmail.com>
2023-08-10 01:59:48 -0400

    Use SSG

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -3,3 +3,7 @@ import React from "react";
 export default function Page({ params }: { params: { id: string } }) {
   return <div>This Blog post ID: {params.id}</div>;
 }
+
+export function generateStaticParams() {
+  return [{ id: "1" }, { id: "2" }, { id: "3" }];
+}
