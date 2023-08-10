01d7bea486cd2105cd9d4371801c63be50baa98b
jerryseigle <mr.jerryseigle@gmail.com>
2023-08-10 14:30:10 -0400

    Fixed header styling

--- src/app/_components/AppHeader.tsx
+++ src/app/_components/AppHeader.tsx
@@ -1,21 +1,18 @@
-import React from "react";
-import Image from "next/image";
 import { ShoppingBasket } from "lucide-react";
+import React from "react";
 
 const AppHeader: React.FC = () => {
   return (
     <header className="bg-[#212529] border-b border-white sm:h-[77px] lg:h-[91px]">
-      <div className="container mx-auto px-4 flex justify-between items-center h-full">
+      <div className="container mx-auto px-10 flex justify-center items-center h-full lg:justify-between max-w-none">
         <div className="flex items-center">
-          <Image
-            src="/path-to-your-logo.png"
+          <img
+            src="https://placehold.co/190x57"
             alt="Logo"
-            width={190}
-            height={57}
             className="sm:w-[166px] sm:h-[50px] lg:w-[190px] lg:h-[57px]"
           />
         </div>
-        <div className="sm:hidden md:flex items-center space-x-4">
+        <div className="hidden lg:flex items-center space-x-8">
           <div className="flex flex-col font-medium text-white font-montserrat">
             <span style={{ fontSize: "12px" }}>1503 Kelly's Landing Dr.</span>
             <span style={{ fontSize: "12px" }}>Mount Holly NC 28120</span>
