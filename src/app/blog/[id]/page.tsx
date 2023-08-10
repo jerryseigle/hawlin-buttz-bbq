99c91dda30e2852ed13b7008e07f43c53fc4447d
jerryseigle <mr.jerryseigle@gmail.com>
2023-08-10 02:01:06 -0400

    New SSG

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 export default function Page({ params }: { params: { id: string } }) {
-  return <div>This Blog post ID: {params.id}</div>;
+  return <div>This My Blog post ID: {params.id}</div>;
 }
 
 export function generateStaticParams() {
