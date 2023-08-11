cd98cd333ff1380e9084b8a7974973d848635ec4
jerryseigle <mr.jerryseigle@gmail.com>
2023-08-10 20:17:25 -0400

    added menu tab bar

---
+++ src/app/_components/MenuTabNavbar.tsx
@@ -0,0 +1,153 @@
+import React from "react";
+
+const MenuTabNavbar = () => {
+  return (
+    <div className="grid grid-cols-1 gap-4 lg:col-span-3">
+      <div className="p-0">
+        <div className="relative">
+          <div className="relative sm:overflow-hidden">
+            <div className="pb-3">
+              <div className="hidden sm:block">
+                <div
+                  className="border-b border-gray-200"
+                  style={{ backgroundColor: "#F2EFEA" }}
+                >
+                  <nav
+                    className="-mb-px flex space-x-8 overflow-x-auto"
+                    aria-label="Tabs"
+                  >
+                    <a
+                      href="#"
+                      className="whitespace-nowrap flex items-center py-4 px-1 border-b-2 font-medium text-sm border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-200"
+                    >
+                      Tab Name with long name
+                      <span className="bg-gray-100 text-gray-900 ml-3 py-0.5 px-2.5 rounded-full text-xs font-medium md:inline-block">
+                        0
+                      </span>
+                    </a>
+
+                    {/* Separator */}
+                    <div className="border-r border-gray-300 h-6 my-auto"></div>
+
+                    {/* Add the remaining tabs and separators */}
+                    <a
+                      href="#"
+                      className="whitespace-nowrap flex items-center py-4 px-1 border-b-2 font-medium text-sm border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-200"
+                    >
+                      Tab Name
+                      <span className="bg-gray-100 text-gray-900 ml-3 py-0.5 px-2.5 rounded-full text-xs font-medium md:inline-block">
+                        1
+                      </span>
+                    </a>
+
+                    <div className="border-r border-gray-300 h-6 my-auto"></div>
+
+                    <a
+                      href="#"
+                      className="whitespace-nowrap flex items-center py-4 px-1 border-b-2 font-medium text-sm border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-200"
+                    >
+                      Tab Name
+                      <span className="bg-gray-100 text-gray-900 ml-3 py-0.5 px-2.5 rounded-full text-xs font-medium md:inline-block">
+                        2
+                      </span>
+                    </a>
+
+                    <div className="border-r border-gray-300 h-6 my-auto"></div>
+
+                    <a
+                      href="#"
+                      className="whitespace-nowrap flex items-center py-4 px-1 border-b-2 font-medium text-sm border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-200"
+                    >
+                      Tab Name
+                      <span className="bg-gray-100 text-gray-900 ml-3 py-0.5 px-2.5 rounded-full text-xs font-medium md:inline-block">
+                        2
+                      </span>
+                    </a>
+
+                    <div className="border-r border-gray-300 h-6 my-auto"></div>
+
+                    <a
+                      href="#"
+                      className="whitespace-nowrap flex items-center py-4 px-1 border-b-2 font-medium text-sm border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-200"
+                    >
+                      Tab Name
+                      <span className="bg-gray-100 text-gray-900 ml-3 py-0.5 px-2.5 rounded-full text-xs font-medium md:inline-block">
+                        2
+                      </span>
+                    </a>
+
+                    <div className="border-r border-gray-300 h-6 my-auto"></div>
+
+                    <a
+                      href="#"
+                      className="whitespace-nowrap flex items-center py-4 px-1 border-b-2 font-medium text-sm border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-200"
+                    >
+                      Tab Name
+                      <span className="bg-gray-100 text-gray-900 ml-3 py-0.5 px-2.5 rounded-full text-xs font-medium md:inline-block">
+                        2
+                      </span>
+                    </a>
+
+                    <div className="border-r border-gray-300 h-6 my-auto"></div>
+
+                    <a
+                      href="#"
+                      className="whitespace-nowrap flex items-center py-4 px-1 border-b-2 font-medium text-sm border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-200"
+                    >
+                      Tab Name
+                      <span className="bg-gray-100 text-gray-900 ml-3 py-0.5 px-2.5 rounded-full text-xs font-medium md:inline-block">
+                        2
+                      </span>
+                    </a>
+
+                    <div className="border-r border-gray-300 h-6 my-auto"></div>
+
+                    <a
+                      href="#"
+                      className="whitespace-nowrap flex items-center py-4 px-1 border-b-2 font-medium text-sm border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-200"
+                    >
+                      Tab Name
+                      <span className="bg-gray-100 text-gray-900 ml-3 py-0.5 px-2.5 rounded-full text-xs font-medium md:inline-block">
+                        2
+                      </span>
+                    </a>
+
+                    <div className="border-r border-gray-300 h-6 my-auto"></div>
+
+                    <a
+                      href="#"
+                      className="whitespace-nowrap flex items-center py-4 px-1 border-b-2 font-medium text-sm border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-200"
+                    >
+                      Tab Name
+                      <span className="bg-gray-100 text-gray-900 ml-3 py-0.5 px-2.5 rounded-full text-xs font-medium md:inline-block">
+                        2
+                      </span>
+                    </a>
+
+                    <div className="border-r border-gray-300 h-6 my-auto"></div>
+
+                    <a
+                      href="#"
+                      className="whitespace-nowrap flex items-center py-4 px-1 border-b-2 font-medium text-sm border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-200"
+                    >
+                      Tab Name
+                      <span className="bg-gray-100 text-gray-900 ml-3 py-0.5 px-2.5 rounded-full text-xs font-medium md:inline-block">
+                        2
+                      </span>
+                    </a>
+
+                    <div className="border-r border-gray-300 h-6 my-auto"></div>
+
+                    {/* Repeat the pattern for other tabs */}
+                  </nav>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MenuTabNavbar;
