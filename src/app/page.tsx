b398cdcf3577fd78d8588f790362eb77eaed82ff
jerryseigle <mr.jerryseigle@gmail.com>
2023-08-12 02:24:21 -0400

    Layout completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,6 @@ export default function Home() {
           <MenuItemCard />
           <MenuItemCard />
         </div>
-        <SectionDivider divId="Appetizerss" title="Appetizers" />;
       </main>
     </div>
   );
