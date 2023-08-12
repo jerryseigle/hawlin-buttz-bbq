22cf55222dde8c7b416148dd8ea12b6da3014979
jerryseigle <mr.jerryseigle@gmail.com>
2023-08-12 14:34:56 -0400

    fixed back link for development

--- src/app/_components/BreadCrumbsNav.tsx
+++ src/app/_components/BreadCrumbsNav.tsx
@@ -12,7 +12,12 @@ export default function BreadcrumbsNav(props: BreadCrumbsNavProps) {
   return (
     <div role="presentation" className="py-2 md:py-4">
       <Breadcrumbs aria-label="breadcrumb">
-        <Link underline="hover" color="inherit" href="/">
+        <Link
+          underline="hover"
+          color="inherit"
+          // TODO: Change the link back to "/" when you move this to production
+          href="https://jerryseigle.github.io/hawlin-buttz-bbq/"
+        >
           <span className="text-red-600 font-bold flex items-center">
             <ChevronLeft />
             BACK TO MENU
