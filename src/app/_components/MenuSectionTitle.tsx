7e4a6884133d3d9e6daae082bdc85aa7c83e7a5f
jerryseigle <mr.jerryseigle@gmail.com>
2023-08-11 20:59:00 -0400

    added div id

--- src/app/_components/MenuSectionTitle.tsx
+++ src/app/_components/MenuSectionTitle.tsx
@@ -1,24 +1,27 @@
 import React from "react";
 import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
 
 interface SectionDividerProps {
+  divId: string;
   title: string;
 }
 
 const SectionDivider = (props: SectionDividerProps) => {
   return (
-    <Typography
-      variant="h1"
-      sx={{
-        textAlign: "center",
-        padding: 4,
-        fontSize: { xs: 28, md: 40 },
-        fontWeight: "bold",
-      }}
-      component="h1"
-    >
-      {props.title}
-    </Typography>
+    <Box id={props.divId} component={"div"} sx={{ padding: 4 }}>
+      <Typography
+        variant="h1"
+        sx={{
+          textAlign: "center",
+          fontSize: { xs: 28, md: 40 },
+          fontWeight: "bold",
+        }}
+        component="h1"
+      >
+        {props.title}
+      </Typography>
+    </Box>
   );
 };
 
